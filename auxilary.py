9b87b8c84267853644d828139d766cc2f2b535ee
Evraa <evramyousef@gmail.com>
2020-06-26 09:51:24 +0200

    separated the common functions in auxilary file, added neighbours, and appropriate check for Forced

---
+++ auxilary.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+def meetTheNeighbours(mat):
+    '''
+    returns the neighbours of each cell
+
+    neighbours:
+        is a list of 9 elements
+        0 -> [0,0]
+        1 -> [0,1]
+        2 -> [0,2]
+        3 -> [1,0]
+        4 -> [1,1]
+        5 -> [1,2]
+        6 -> [2,0]
+        7 -> [2,1]
+        8 -> [2,2]
+    '''
+    neighbours = []
+    neighbours.append([ [[0,1],[0,2]], [[1,0],[2,0]], [[1,1],[2,2]] ])
+    neighbours.append([ [[1,1],[2,1]], [[0,0],[0,2]]])
+    neighbours.append([ [[0,1],[0,0]], [[1,2],[2,2]], [[1,1],[2,0]] ])
+    neighbours.append([ [[1,1],[1,2]], [[0,0],[2,0]]])
+    neighbours.append([ [[0,1],[2,1]], [[1,0],[1,2]], [[0,0],[2,2]], [[0,2],[2,0]] ])
+    neighbours.append([ [[1,1],[1,0]], [[0,2],[2,2]]])
+    neighbours.append([ [[0,0],[1,0]], [[2,1],[2,2]], [[1,1],[0,2]] ])
+    neighbours.append([ [[2,0],[2,2]], [[0,1],[1,1]]])
+    neighbours.append([ [[2,1],[2,0]], [[1,2],[0,2]], [[1,1],[0,0]] ])
+
+    return neighbours
+
+def checkForMagic (mat):
+    '''
+    Return magic if exist, othw. returns false
+    dummy operation
+    '''
+    #row 1
+    if mat[0,0]!=0 and mat[0,1]!=0 and mat[0,2]!=0:
+        return mat[0,0] + mat[0,1] + mat[0,2]
+    #row 2
+    if mat[1,0]!=0 and mat[1,1]!=0 and mat[1,2]!=0:
+        return mat[1,0] + mat[1,1] + mat[1,2]
+    #row 3
+    if mat[2,0]!=0 and mat[2,1]!=0 and mat[2,2]!=0:
+        return mat[2,0] + mat[2,1]!=0 + mat[2,2]
+    #col 1
+    if mat[0,0]!=0 and mat[1,0]!=0 and mat[2,0]!=0:
+        return mat[0,0] + mat[1,0] + mat[2,0]
+    #col 2
+    if mat[0,1]!=0 and mat[1,1]!=0 and mat[2,1]!=0:
+        return mat[0,1] + mat[1,1] + mat[2,1]
+    #col 3
+    if mat[0,2]!=0 and mat[1,2]!=0 and mat[2,2]!=0:
+        return mat[0,2] + mat[1,2] + mat[2,2]
+    #diagonal right
+    if mat[0,0]!=0 and mat[1,1]!=0 and mat[2,2]!=0:
+        return mat[0,0] + mat[1,1] + mat[2,2]
+    #diagonal left
+    if mat[0,2]!=0 and mat[1,1]!=0 and mat[2,0]!=0:
+        return mat[0,2] + mat[1,1] + mat[2,0]
+    
+    return False
+
+
+def check (mat, magic=0):
+    '''
+    Checks if this matrix does not break any of the restrictions
+    '''
+    #count uniques
+    npn_zero_elements = []
+    for i in mat:
+        for j in i:
+            if j!=0:
+                npn_zero_elements.append(j)
+
+    unique_elements = len(np.unique(npn_zero_elements))
+    if unique_elements != len(npn_zero_elements) :
+        return False
+
+    #Check row summation is the same
+    sum_x = mat.sum(axis=0)
+    for element in sum_x:
+        if element > magic:
+            return False
+    
+    #Check column summation is the same
+    sum_y = mat.sum(axis=1)
+    for element in sum_y:
+        if element > magic:
+            return False
+
+    #check diagonals
+    if mat[0,0]+mat[1,1]+mat[2,2] > magic or mat[0,2]+mat[1,1]+mat[2,0] > magic:
+        return False    
+    return True
+
+def checkForForced (mat, neighbours, magic):
+    '''
+    return the index of one of the FORCED cells
+    if non exist, returns False
+    '''
+    n,m = mat.shape
+    for i in range(n):
+        for j in range(m):
+            if mat[i,j] == 0:
+                #Check for its neighbours
+                idx = (i*3) + j
+                for neighbour in neighbours[idx]:
+                    neighbour_0 = neighbour[0]
+                    neighbour_1 = neighbour[1]
+                    if mat[neighbour_0[0]][[neighbour_0[1]]] != 0 and\
+                        mat[neighbour_1[0]][[neighbour_1[1]]] != 0:
+                        return i,j,(magic - mat[neighbour_0[0]][[neighbour_0[1]]] -\
+                            mat[neighbour_1[0]][[neighbour_1[1]]])
+    return False
--- solve.py
+++ solve.py
@@ -3,78 +3,39 @@
 - Fixed values are the ones that are non-zeors
 '''
 import numpy as np
-
-
-def check (mat, magic=0):
-    '''
-    Checks if this matrix obey the rules (unique elements/summation)
-    '''
-    #count uniques
-    npn_zero_elements = []
-    for i in mat:
-        for j in i:
-            if j!=0:
-                npn_zero_elements.append(j)
-
-    unique_elements = len(np.unique(npn_zero_elements))
-    if unique_elements != len(npn_zero_elements) :
-        return False
-
-    #Check row summation is the same
-    sum_x = mat.sum(axis=0)
-    for element in sum_x:
-        if element > magic:
-            return False
-    
-    #Check column summation is the same
-    sum_y = mat.sum(axis=1)
-    for element in sum_y:
-        if element > magic:
-            return False
-
-    #check diagonals
-    if mat[0,0]+mat[1,1]+mat[2,2] > magic or mat[0,2]+mat[1,1]+mat[2,0] > magic:
-        return False    
-    return True
-
-def checkForMagic (mat):
-    '''
-    Return magic if exist, othw. returns false
-    dummy operation
-    '''
-
-    if mat[0,0]!=0 and mat[0,1]!=0 and mat[0,2]!=0:
-        return mat[0,0] + mat[0,1] + mat[0,2]
-
-    if mat[1,0]!=0 and mat[1,1]!=0 and mat[1,2]!=0:
-        return mat[1,0] + mat[1,1] + mat[1,2]
-
-    if mat[2,0]!=0 and mat[2,1]!=0 and mat[2,2]!=0:
-        return mat[2,0] + mat[2,1]!=0 + mat[2,2]
-
-    if mat[0,0]!=0 and mat[1,0]!=0 and mat[2,0]!=0:
-        return mat[0,0] + mat[1,0] + mat[2,0]
-
-    if mat[0,1]!=0 and mat[1,1]!=0 and mat[2,1]!=0:
-        return mat[0,1] + mat[1,1] + mat[2,1]
-
-    if mat[0,2]!=0 and mat[1,2]!=0 and mat[2,2]!=0:
-        return mat[0,2] + mat[1,2] + mat[2,2]
-
-    if mat[0,0]!=0 and mat[1,1]!=0 and mat[2,2]!=0:
-        return mat[0,0] + mat[1,1] + mat[2,2]
-
-    if mat[0,2]!=0 and mat[1,1]!=0 and mat[2,0]!=0:
-        return mat[0,2] + mat[1,1] + mat[2,0]
-    
-    return False
+from auxilary import *
 
 
 
 def solve(mat):
-    print (checkForMagic(mat))
+    '''
+    The main Calling function with the algorithm.
+
+    Algorithm:
+        + Assign needed data (Neigh,...)
+        + Check if we have a Magic
+            + if so, loop until all forced elements are filled
+            + if not done yet, TODO: add options
+        + if no magic, TODO: add options
+    '''
+
+    #Assign data
+    Neigh = meetTheNeighbours(mat)
+
+    #Check for magic
+    magic = checkForMagic(mat)
+
+    if magic:
+        forced_idx_x,forced_idx_y,val = checkForForced(mat,Neigh,magic)
+        
+        print (forced_idx_x,forced_idx_y,val)
 
 if __name__ == "__main__":
+    #GLOBAL VARIABLES
+    # orig_mat = np.zeros([3,3])
+    # magic = 0
+
+
     #mat = np.arange(9).reshape(3, 3)
     mat = np.zeros([3,3])
     mat[0,0] = 2
